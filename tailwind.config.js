daba84fcc991ec89498ac486a02125b0f73092cf
habibi55 <muhamadhabibi14168@gmail.com>
2022-03-30 16:24:59 +0700

    bismillah

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        body: "#17171F",
+        primer: "#F9FAF4",
+        secondary: "#F17A7E",
+        tersier: "#3c3f58",
+        nav: "#0072AE",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
