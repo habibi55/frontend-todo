3265e0c8cc896fb15537fcd54986a32bed0b4015
habibi55 <muhamadhabibi14168@gmail.com>
2022-06-18 00:06:07 +0700

    bikin landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     extend: {
       colors: {
         body: "#17171F",
-        primer: "#F9FAF4",
+        primer: "#3AAFA9",
+        primer_2: "#2B7A78",
         secondary: "#F17A7E",
         tersier: "#3c3f58",
         nav: "#0072AE",
