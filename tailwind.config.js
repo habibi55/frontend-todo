94919a848b3303a6065737f46b2c7cc6b00d7bbc
habibi55 <muhamadhabibi14168@gmail.com>
2022-06-30 23:29:34 +0700

    setting detail profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         body: "#17171F",
         primer: "#3AAFA9",
         primer_2: "#2B7A78",
+        primer_3: "#3E4785",
+        primer_3_hover: "#32396a",
         secondary: "#F17A7E",
         tersier: "#3c3f58",
         nav: "#0072AE",
