e5f68017039c273c5e1d007b62c3c7a6b14ac5ef
Muhamad Habibi <muhamadhabibi14168@gmail.com>
2022-07-01 21:56:50 +0700

    revisi profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         primer: "#3AAFA9",
         primer_2: "#2B7A78",
         primer_3: "#3E4785",
-        primer_3_hover: "#32396a",
+        primer_3_hover: "#252b50",
         secondary: "#F17A7E",
         tersier: "#3c3f58",
         nav: "#0072AE",
